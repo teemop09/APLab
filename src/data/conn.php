84d5a3bf3cb239b9e3913341d624c73232fb90da
Jin Ann <101621295+teemop09@users.noreply.github.com>
2023-09-08 04:49:30 +0800

    Added feature to view ticket history for TA and standard users. (#5)

--- src/data/conn.php
+++ src/data/conn.php
@@ -3,7 +3,7 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . '/config.php';
 
 // Create a connection
-$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
 
 // Check connection
 if ($conn->connect_error) {
