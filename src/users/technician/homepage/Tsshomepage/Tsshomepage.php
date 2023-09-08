0166f20d427862c0804f69e4cd3e177204443fa6
Jin Ann <jinann030613@hotmail.com>
2023-09-08 09:37:10 +0800

    Added TSS homepage.

--- src/users/technician/homepage/Tsshomepage/Tsshomepage.php
+++ src/users/technician/homepage/Tsshomepage/Tsshomepage.php
@@ -10,8 +10,9 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/src/data/conn.php';
   <meta property="og:title" content="TssHomepage" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <meta charset="utf-8" />
-  <link rel="stylesheet" href="globals.css" />
-  <link rel="stylesheet" href="style.css" />
+  <!-- <link rel="stylesheet" href="globals.css" />
+  <link rel="stylesheet" href="style.css" /> -->
+  <link rel="stylesheet" href="tssHomepage.css" />
 </head>
 
 <body>
@@ -26,78 +27,83 @@ require_once $_SERVER['DOCUMENT_ROOT'] . '/src/data/conn.php';
     <script src="TssLoadFooter.js"></script>
     </div>-->
 
-  <div class="HOMEPAGE">
-    <div class="header-footer-bg">
 
-      <?php
-      $result = mysqli_query($conn, "SELECT * FROM technician_t");
-      $row = mysqli_fetch_array($result);
+  <?php
+  $result = mysqli_query($conn, "SELECT * FROM technician_t");
+  $row = mysqli_fetch_array($result);
 
-      ?>
-
-      <?php
-      echo '<div class="welcome">';
-      echo 'WELCOME BACK, ' . $row['tech_first_name'] . '!';
-      ?>
-
-      <a href="/ticket/ticket_to_pending.php">
-        <div class="pending-ticket">
-          <div class="overlap-2">
-            <div class="pendingTicketText">Pending Ticket</div>
-            <img class="pendingTicketImg" src="/src/assets/Logos/pending ticket.png" />
-          </div>
+  ?>
+  <div class="homepage">
+    <div class="greeting">
+      <h2>WELCOME BACK,
+        <?= strtoupper($row["tech_first_name"]) ?>
+      </h2>
+    </div>
+    <div id="action-container">
+      <div class="row">
+        <div class="column">
+          <a href="/src/users/technician/rounding/pending_ticket.php">
+            <div class="overlap-2">
+              <div class="pendingTicketText">Pending Tickets</div>
+              <img class="pendingTicketImg" src="/src/assets/Logos/pending ticket.png" />
+            </div>
+          </a>
         </div>
-      </a>
-
-      <a href="/ticket/all_ticket_history.php">
-        <div class="ticket-history">
-          <div class="overlap-2">
-            <div class="ticketHistoryText">Ticket History</div>
-            <img class="ticketHistoryImg" src="/src/assets/Logos/ticket history.png" />
-          </div>
+        <div class="column">
+          <a href="/src/users/technician/ticket/all_ticket_history.php">
+            <div class="ticket-history">
+              <div class="overlap-2">
+                <div class="ticketHistoryText">Ticket History</div>
+                <img class="ticketHistoryImg" src="/src/assets/Logos/ticket history.png" />
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
-
-      <a href="/rounding/view_layout_home.php">
-        <div class="view-lab-layout">
-          <div class="overlap-2">
-            <div class="viewLabLayoutText">View Lab Layout</div>
-            <img class="viewLabLayoutImg" src="/src/assets/Logos/view lab layout.png" />
-          </div>
+        <div class="column">
+          <a href="/src/users/technician/rounding/view_layout_home.php">
+            <div class="view-lab-layout">
+              <div class="overlap-2">
+                <div class="viewLabLayoutText">View Lab Layout</div>
+                <img class="viewLabLayoutImg" src="/src/assets/Logos/view lab layout.png" />
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
-
-      <!-- dont know which path -->
-      <a href="">
-        <div class="raise-ticket">
-          <div class="overlap-2">
-            <div class="raiseTicketText">Raise Ticket</div>
-            <img class="raiseTicketImg" src="/src/assets/Logos/raise ticket.png" />
-          </div>
+      </div>
+      <div class="row">
+        <div class="column">
+          <a href="/src/users/standard-user/raise-ticket/submitTicket.php">
+            <div class="raise-ticket">
+              <div class="overlap-2">
+                <div class="raiseTicketText">Raise Ticket</div>
+                <img class="raiseTicketImg" src="/src/assets/Logos/raise ticket.png" />
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
 
-      <!-- dont know which path -->
-      <a href="">
-        <div class="register-client">
-          <div class="overlap-2">
-            <div class="registerClientText">Register Account (Student/Lecturer)</div>
-            <img class="registerAccountImg" src="/src/assets/Logos/register account.png" />
-          </div>
+        <div class="column">
+          <a href="/src/users/technician/register/RegisterAccountUser.html">
+            <div class="register-client">
+              <div class="overlap-2">
+                <div class="registerClientText">Register Account (Student/Lecturer)</div>
+                <img class="registerAccountImg" src="/src/assets/Logos/register account.png" />
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
 
-      <!-- dont know which path -->
-      <a href="">
-        <div class="resgiter-TA">
-          <div class="overlap-2">
-            <div class="registerTAText">Register Account (Technical Staff)</div>
-            <img class="registerAccountImg" src="/src/assets/Logos/register account.png" />
-          </div>
+        <div class="column">
+          <a href="/src/users/technician/register/RegisterAccountTechnician.html">
+            <div class="resgiter-TA">
+              <div class="overlap-2">
+                <div class="registerTAText">Register Account (Technical Staff)</div>
+                <img class="registerAccountImg" src="/src/assets/Logos/register account.png" />
+              </div>
+            </div>
+          </a>
         </div>
-      </a>
-
-
+      </div>
     </div>
 </body>
 
